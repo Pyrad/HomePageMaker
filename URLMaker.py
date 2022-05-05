b0b907150dc36f06d08a6765adeb7d50297c471d
Pyrad <cailong89@163.com>
2022-05-05 20:07:07 +0800

    Move run code to a static method

--- URLMaker.py
+++ URLMaker.py
@@ -171,6 +171,63 @@ class URLMaker:
     </div>
 '''.format(url, url, imgf, webname, descri)
 
+    def get_section_sets(self):
+        # First check if the URL list file exists
+        fname = self.url_file
+        if not URLMaker.check_file_exists(fname):
+            print("Error, file not found:", fname)
+            return
+
+        url_pattern = re.compile(r'data-url="(.*)"')
+        title_pattern = re.compile(r'title="(.*)"')
+        img_pattern = re.compile(r'image="(.*)"')
+
+        section_dict = dict()
+        section_dict['__NOSECTION__'] = []
+
+        line_cnt = 0
+        f = open(fname, 'r', encoding='utf-8')
+
+        cur_sect_name = '__NOSECTION__'
+        for line in f.readlines():
+            line_cnt += 1
+            l = line.strip()
+            if len(l) == 0:
+                continue
+            if l.startswith("<!--"):
+                llist = l.split()
+                if len(llist) == 3:
+                    cur_sect_name = llist[1]
+                    section_dict[cur_sect_name] = []
+                continue
+            if not l.startswith("data-url"):
+                continue
+
+            kwlist = l.split(",")
+            assert len(kwlist) == 2 or len(kwlist) == 3
+
+            url_m = url_pattern.match(kwlist[0].strip())
+            title_m = title_pattern.match(kwlist[1].strip())
+            img_m = img_pattern.match(kwlist[2].strip()) if len(kwlist) == 3 else None
+
+            if url_m is None or title_m is None:
+                continue
+            str_url = url_m.group(1)
+            str_webname = title_m.group(1)
+            str_imgf = "undef128x128.png" if img_m is None else img_m.group(1)
+            str_imgf = "undef128x128.png" if len(str_imgf) == 0 else str_imgf
+            str_descri = str_webname
+
+            csect = section_dict[cur_sect_name]
+            csect.append([str_url, str_webname, str_imgf, str_descri])
+
+        f.close()
+
+        for sname, msglist in section_dict.items():
+            print("Section {} has {} items".format(sname, len(msglist)))
+            for urlstr in msglist:
+                print("\t", urlstr)
+
     def generate_index_html(self, fname="index.html", body_file="url.cols.html"):
         if not URLMaker.check_file_exists(self.index_file_head):
             print("[ERROR] The head part (file) for the final index.html doesn't exist")
@@ -259,25 +316,29 @@ class URLMaker:
         if already_exist_cnt != 0:
             print("[WARNING] {} icon already exists in destination directory, skip copy".format(already_exist_cnt))
 
+    @staticmethod
+    def default_run():
+        url_list_file = "classified_urls.html"
+        str_imgf = "undef.png"
+        umkr = URLMaker(url_list_file=url_list_file, default_img=str_imgf)
 
+        umkr.generate_index_html("index.html")
+
+        do_copy = True
+        #webStackPageDir = "D:/Pyrad/WebHomePage/WebStackPage.github.io-master"
+        webStackPageDir = "D:/Programs/TempDownload/WebStackPage.github.io-master/WebStackPage.github.io-master"
+        webStackPageDir_snps = "C:/Users/longc/Downloads/WebStackPage.github.io-master"
+
+        use_default_pre = True
+        if do_copy:
+            path_pre = webStackPageDir if use_default_pre else webStackPageDir_snps
+            icon_dest = path_pre + "/assets/images/logos"
+            index_html_dest = path_pre + "/cn"
+            umkr.do_copy(icon_dest=icon_dest, index_html_dest=index_html_dest)
 
 
 if __name__ == "__main__":
-    url_list_file = "classified_urls.html"
-    str_imgf= "undef.png"
-    umkr = URLMaker(url_list_file=url_list_file, default_img=str_imgf)
+    URLMaker.default_run()
 
-    umkr.generate_index_html("index.html")
-
-    do_copy = True
-    webStackPageDir = "D:/Pyrad/WebHomePage/WebStackPage.github.io-master"
-    webStackPageDir_snps = "C:/Users/longc/Downloads/WebStackPage.github.io-master"
-
-    use_default_pre = True
-    if do_copy:
-        path_pre = webStackPageDir if use_default_pre else webStackPageDir_snps
-        icon_dest = path_pre + "/assets/images/logos"
-        index_html_dest = path_pre + "/cn"
-        umkr.do_copy(icon_dest=icon_dest, index_html_dest=index_html_dest)
 
 
