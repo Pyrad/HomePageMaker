66450a0918ef620b2834ba483d51f5bdfeed0689
Pyrad <cailong89@163.com>
2022-05-06 23:04:03 +0800

    Add some functions to generate new index.html

--- URLMaker.py
+++ URLMaker.py
@@ -173,6 +173,113 @@ class URLMaker:
     </div>
 '''.format(url, url, imgf, webname, descri)
 
+    def get_page_head_raw_text(self):
+        return  '<head>\n'\
+                '\t<meta charset="utf-8">\n'\
+                '\t<meta http-equiv="X-UA-Compatible" content="IE=edge">\n'\
+                '\t<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n'\
+                '\t<meta name="author" content="viggo" />\n'\
+                '\t<title>Pyrad Home</title>\n'\
+                '\t<meta name="keywords" content="Site Navigation Main Page - keywords">\n'\
+                '\t<meta name="description" content="Site Navigation Main Page - description">\n'\
+                '\t<link rel="shortcut icon" href="../assets/images/favicon.png">\n'\
+                '\t<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Arimo:400,700,400italic">\n'\
+                '\t<link rel="stylesheet" href="../assets/css/fonts/linecons/css/linecons.css">\n'\
+                '\t<link rel="stylesheet" href="../assets/css/fonts/fontawesome/css/font-awesome.min.css">\n'\
+                '\t<link rel="stylesheet" href="../assets/css/bootstrap.css">\n'\
+                '\t<link rel="stylesheet" href="../assets/css/xenon-core.css">\n'\
+                '\t<link rel="stylesheet" href="../assets/css/xenon-components.css">\n'\
+                '\t<link rel="stylesheet" href="../assets/css/xenon-skins.css">\n'\
+                '\t<link rel="stylesheet" href="../assets/css/nav.css">\n'\
+                '\t<script src="../assets/js/jquery-1.11.1.min.js"></script>\n'\
+                '\t<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->\n'\
+                '\t<!--[if lt IE 9]>\n'\
+                '\t\t<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>\n'\
+                '\t\t<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>\n'\
+                '\t<![endif]-->\n'\
+                '\t<!-- / FB Open Graph -->\n'\
+                '\t<meta property="og:type" content="article">\n'\
+                '\t<meta property="og:url" content="http://www.webstack.cc/">\n'\
+                '\t<meta property="og:title" content="Site Navigation og:title">\n'\
+                '\t<meta property="og:description" content="Site Navigation og:description">\n'\
+                '\t<meta property="og:image" content="http://webstack.cc/assets/images/webstack_banner_cn.png">\n'\
+                '\t<meta property="og:site_name" content="Site Navigation og:site_name">\n'\
+                '\t<!-- / Twitter Cards -->\n'\
+                '\t<meta name="twitter:card" content="summary_large_image">\n'\
+                '\t<meta name="twitter:title" content="Site Navigation twitter:title">\n'\
+                '\t<meta name="twitter:description" content="Site Navigation twitter:description">\n'\
+                '\t<meta name="twitter:image" content="http://www.webstack.cc/assets/images/webstack_banner_cn.png">\n'\
+                '</head>'
+
+    def get_page_body_script_raw_text(self):
+        return  '\t<!-- 锚点平滑移动 -->\n'\
+                '\t<script type="text/javascript">\n'\
+                '\t$(document).ready(function() {\n'\
+                '\t\t//img lazy loaded\n'\
+                '\t\tconst observer = lozad();\n'\
+                '\t\tobserver.observe();\n'\
+                '\t\t$(document).on(\'click\', \'.has-sub\', function(){\n'\
+                '\t\t\tvar _this = $(this)\n'\
+                '\t\t\tif(!$(this).hasClass(\'expanded\')) {\n'\
+                '\t\t\t\tsetTimeout(function(){_this.find(\'ul\').attr("style","")}, 300);\n'\
+                '\t\t\t} else {\n'\
+                '\t\t\t\t$(\'.has-sub ul\').each(function(id,ele){\n'\
+                '\t\t\t\t\tvar _that = $(this)\n'\
+                '\t\t\t\t\tif(_this.find(\'ul\')[0] != ele) {\n'\
+                '\t\t\t\t\t\tsetTimeout(function(){_that.attr("style","")}, 300);\n'\
+                '\t\t\t\t\t}\n'\
+                '\t\t\t\t})\n'\
+                '\t\t\t}\n'\
+                '\t\t})\n'\
+                '\t\t$(\'.user-info-menu .hidden-sm\').click(function(){\n'\
+                '\t\t\tif($(\'.sidebar-menu\').hasClass(\'collapsed\')) {\n'\
+                '\t\t\t\t$(\'.has-sub.expanded > ul\').attr("style","")\n'\
+                '\t\t\t} else {\n'\
+                '\t\t\t\t$(\'.has-sub.expanded > ul\').show()\n'\
+                '\t\t\t}\n'\
+                '\t\t})\n'\
+                '\t\t$("#main-menu li ul li").click(function() {\n'\
+                '\t\t\t$(this).siblings(\'li\').removeClass(\'active\'); // 删除其他兄弟元素的样式\n'\
+                '\t\t\t$(this).addClass(\'active\'); // 添加当前元素的样式\n'\
+                '\t\t});\n'\
+                '\t\t$("a.smooth").click(function(ev) {\n'\
+                '\t\t\tev.preventDefault();\n'\
+                '\t\t\tpublic_vars.$mainMenu.add(public_vars.$sidebarProfile).toggleClass(\'mobile-is-visible\');\n'\
+                '\t\t\tps_destroy();\n'\
+                '\t\t\t$("html, body").animate({\n'\
+                '\t\t\t\tscrollTop: $($(this).attr("href")).offset().top - 30\n'\
+                '\t\t\t}, {\n'\
+                '\t\t\t\tduration: 500,\n'\
+                '\t\t\t\teasing: "swing"\n'\
+                '\t\t\t});\n'\
+                '\t\t});\n'\
+                '\t\treturn false;\n'\
+                '\t});\n'\
+                '\t\n'\
+                '\tvar href = "";\n'\
+                '\tvar pos = 0;\n'\
+                '\t$("a.smooth").click(function(e) {\n'\
+                '\t\t$("#main-menu li").each(function() {\n'\
+                '\t\t\t$(this).removeClass("active");\n'\
+                '\t\t});\n'\
+                '\t\t$(this).parent("li").addClass("active");\n'\
+                '\t\te.preventDefault();\n'\
+                '\t\thref = $(this).attr("href");\n'\
+                '\t\tpos = $(href).position().top - 30;\n'\
+                '\t});\n'\
+                '\t</script>\n'\
+                '\t<!-- Bottom Scripts -->\n'\
+                '\t<script src="../assets/js/bootstrap.min.js"></script>\n'\
+                '\t<script src="../assets/js/TweenMax.min.js"></script>\n'\
+                '\t<script src="../assets/js/resizeable.js"></script>\n'\
+                '\t<script src="../assets/js/joinable.js"></script>\n'\
+                '\t<script src="../assets/js/xenon-api.js"></script>\n'\
+                '\t<script src="../assets/js/xenon-toggles.js"></script>\n'\
+                '\t<!-- JavaScripts initializations and stuff -->\n'\
+                '\t<script src="../assets/js/xenon-custom.js"></script>\n'\
+                '\t<script src="../assets/js/lozad.js"></script>\n'
+
+
     def get_col_raw_text(self, url, imgf, webname, descri):
         return  '\t\t\t\t<div class="col-sm-3">\n'\
                 '\t\t\t\t\t<div class="xe-widget xe-conversations box2 label-info" onclick="window.open(\'{}\', \'_blank\')" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="{}">\n'\
@@ -372,8 +479,29 @@ class URLMaker:
 
         return main_content_lines
 
+    def get_page_container(self, biglogo, smalllogo):
+        slines = ['\t<!-- skin-white -->', '\t<div class ="page-container" >']
+        slines.extend(self.get_side_bar_menu(biglogo=biglogo, smalllogo=smalllogo))
+        slines.extend(self.get_main_content())
+        slines.append('\t</div>')
 
+        return slines
 
+    def get_page_body(self, biglogo, smalllogo):
+        slines = ['<body class="page-body">']
+        slines.extend(self.get_page_container(biglogo=biglogo, smalllogo=smalllogo))
+        slines.append(self.get_page_body_script_raw_text())
+        slines.append('</body>')
+
+        return slines
+
+    def get_index_html(self, biglogo, smalllogo):
+        slines = ['<!DOCTYPE html>', '<html lang="zh">']
+        slines.append(self.get_page_head_raw_text())
+        slines.extend(self.get_page_body(biglogo=biglogo, smalllogo=smalllogo))
+        slines.append('</html>')
+
+        return slines
 
     def get_section_sets(self):
         # First check if the URL list file exists
@@ -536,8 +664,11 @@ class URLMaker:
         sline_file = 'sline_tmp.html'
         fname = testdir + '/' + sline_file
 
-        slines = umkr.get_side_bar_menu(biglogo=logo_big, smalllogo=logo_small)
-        slines.extend(umkr.get_main_content())
+        #slines = umkr.get_side_bar_menu(biglogo=logo_big, smalllogo=logo_small)
+        #slines.extend(umkr.get_main_content())
+
+        #slines = umkr.get_page_container(biglogo=logo_big, smalllogo=logo_small)
+        slines = umkr.get_index_html(biglogo=logo_big, smalllogo=logo_small)
 
         output_lines = "\n".join(slines)
         with open(fname, "w", encoding='utf-8') as fw:
