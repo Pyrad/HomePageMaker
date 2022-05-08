994c31ef23f25ce9fd1a952bb4c07b39131aef55
pyrad <cailong89@163.com>
2022-05-08 19:32:58 +0800

    Remove unused functions

--- URLMaker.py
+++ URLMaker.py
@@ -73,25 +73,6 @@ class URLMaker:
         print(clma_Back.LIGHTYELLOW_EX + '[WARNING]', end="")
         print('', warnmsgstr)
 
-    def col_raw_text(self, url, imgf, webname, descri):
-        return \
-'''    <div class="col-sm-3">
-        <div class="xe-widget xe-conversations box2 label-info" onclick="window.open('{}', '_blank')" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="{}">
-            <div class="xe-comment-entry">
-                <a class="xe-user-img">
-                    <img data-src="../assets/images/logos/{}" class="lozad img-circle" width="40">
-                </a>
-                <div class="xe-comment">
-                    <a href="#" class="xe-user-name overflowClip_1">
-                        <strong>{}</strong>
-                    </a>
-                    <p class="overflowClip_2">{}</p>
-                </div>
-            </div>
-        </div>
-    </div>
-'''.format(url, url, imgf, webname, descri)
-
     def get_page_head_raw_text(self):
         return  '<head>\n'\
                 '\t<meta charset="utf-8">\n'\
